eb0b0d87fe5ed066290019deca8d0bc8c7bd8c19
28kayak <kaya_ota@msn.com>
2015-03-10 18:29:28 -0700

    update

--- sampleSyntax.rkt
+++ sampleSyntax.rkt
@@ -14,13 +14,43 @@
 (sqrt(* 2 8))
 
 (define (quadratic a b c) 
-(/ (+ (* b -1) (sqrt (-(* b b) (*(* 4 a)c))) ) (* 2 a))
+  (/ (+ (* b -1) (sqrt (-(* b b) (*(* 4 a)c))) ) (* 2 a)) 
 );define 
 
-(quadratic 1 4 1)
 
 
 (define (quadratic1 a b c) 
-(/ (- (* b -1) (sqrt (-(* b b) (*(* 4 a)c))) ) (* 2 a))
+ (/ (- (* b -1) (sqrt (-(* b b) (*(* 4 a)c))) ) (* 2 a))
 );define 
-(quadratic1 1 4 1) 
+(quadratic1 1 5 -6) 
+
+(define y (quadratic 1 5 -6))
+(define z (quadratic1 1 5 -6))
+
+(define (quadratic_solution a b c)
+  (if (< (* b b) (*(* 4 a)c))
+      "No solution"
+      (cons (quadratic a b c) (quadratic1 a b c)))
+);define 
+
+(quadratic_solution 1 -2 1)
+
+
+(define (comp x)
+(cond [(> x 3) "bigger"]
+      [(= x 3) "equal"]
+      [(< x 3) "samller "]
+      [else "Oops! somethig worng!"])
+);define comp 
+(comp 7)
+(comp 1)
+(comp 3)
+
+
+
+
+
+(define f (cons (cons (1 2) 3)))
+f
+
+

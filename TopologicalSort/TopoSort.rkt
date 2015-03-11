532d368ba9f7312b122a4069233fadbc10eb88dc
28kayak <kaya_ota@msn.com>
2015-03-10 18:52:47 -0700

    update

--- TopologicalSort/TopoSort.rkt
+++ TopologicalSort/TopoSort.rkt
@@ -2,3 +2,24 @@
 ;Topological sort 
 (define graph '((1 2 3 4) (2 4 5) (3 6) (4 3 6 7) (5 4 7) (6) (7 6)))
 graph
+
+(define (toposort graph) 
+  (define count 1);int count = 0
+  (define skip #f);boolean skip = false 
+  (define j 0);int j = 0 
+  j
+  count 
+  skip
+  
+  
+  
+  
+  
+  
+  
+  
+  
+);define 
+(toposort graph)
+(define x 0)
+x
